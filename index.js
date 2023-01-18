94bc3794c7b51b3eb43264608f715d7f36cc8fe6
Jonathan Bernal <jonathanbernal@users.noreply.github.com>
2023-01-17 21:27:26 -0500

    Add basic structure for the grid but with rendering issues

--- index.js
+++ index.js
@@ -2,27 +2,41 @@ const sliderValue = document.querySelector('.slider-value');
 const gridSlider = document.querySelector('#grid-slider');
 const gridContainer = document.querySelector('.grid-container');
 
-// Render default value for the slider
-sliderValue.textContent = gridSlider.value;
+const DEFAULT_GRID_ITEMS = 16; // This creates a default 16 x 16 grid
+
 
 /**
  *  This function renders a grid on the main page. It resizes itself
  *  based on the number of rows and columns.
  * 
- *  Since our grids are always NxN size, rows and cols are always the same.
- *  However, if you feel like reusing the function for a different project,
- *  you can do so.
+ *  We're always creating a NxN grid, hence the number of columns will be the
+ *  same as that of rows.
  * 
- * @param {Integer} rows the number of rows to generate
- * @param {Integer} cols the number of columns to generate
+ * @param {Integer} rows the number of rows/columns to generate
  */
 function renderGrid(rows, cols){
 
+    const containerWidth = gridContainer.clientWidth;
+    const containerHeight = gridContainer.clientHeight;
+    const rowSize = containerHeight / rows;
+    const columnSize = containerWidth / cols;
+
+    gridContainer.style['grid-template-columns'] = `repeat(auto-fill, minmax(${columnSize}, 1fr))`;
+    gridContainer.style['grid-template-rows'] = `repeat(auto-fill, minmax(${rowSize}, 1fr))`;
+
+    for (let row = 0; row < rows; row++){
+        let gridRow = document.createElement('div');
+
+        for (let col = 0; col < cols; col++) {
+            let gridCol = document.createElement('div');
+            gridCol.classList.add('grid-tile');
+            gridCol.style.width = `${columnSize}px`;
+            gridCol.style.height = `${rowSize}px`;
+            gridRow.appendChild(gridCol);
+        }
+
+        gridContainer.appendChild(gridRow);
+    }
 }
 
-/**
- * This event listener updates the value of the slider and renders it on the webpage.
- */
-gridSlider.addEventListener('input', (evt) => {
-    sliderValue.textContent = evt.target.value;
-});
+renderGrid(30, 30);
